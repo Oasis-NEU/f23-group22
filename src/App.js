fa1c608e1ade67b6d0736824a98b09efe6a3a3f1
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2023-10-15 13:26:38 -0400

    Added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Navigation/Navbar.js";
 
-function App() {
+
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
   return (
-    <div>
-      <Navbar />
+    <div className="App ">
+        <Header />
+        <Navbar />
     </div>
+   
   );
 }
 
-export default App;
+export function Header() {
+  return ( 
+    <header>
+        <h1 className="App-main"> Welcome the Student Business Hub </h1>
+        <h2 >Placeholder text...............</h2>
+      </header>
+
+  )
+}
+
