7cd34d31f49857032aee7e04d88515bed5e024ab
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2023-10-15 13:20:43 -0400

    Added broken menu bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Navigation/Navbar.js";
 
-export default function App() {
+function App() {
   return (
-    <div className="App ">
-        <Header />
+    <div>
+      <Navbar />
     </div>
-   
   );
 }
 
-export function Header() {
-  return ( 
-    <header>
-        <h1 className="App-main"> Welcome the Student Business Hub </h1>
-        <h2 >Placeholder text...............</h2>
-      </header>
-
-  )
-}
-
-
-
-
+export default App;
