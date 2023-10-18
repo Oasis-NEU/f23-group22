b60160c1735210628804686ccae44614aa762882
emmawonng <wong.emma@northeastern.edu>
2023-10-17 22:36:29 -0400

    added new page
    added new page for creating business listings - currently nothing on the page though

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import CreateBusinessListing from './pages/CreateBusinessListing';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "createbusinesslisting",
+    element: <CreateBusinessListing/>,
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
